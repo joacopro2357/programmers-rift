138510d57ff026aca0605c310f6fd65dc0a65b11
joacopro2357 <148890915+joacopro2357@users.noreply.github.com>
2023-10-25 11:51:04 -0300

    commit nashe

---
+++ murk wolves/correcion.js
@@ -0,0 +1,6 @@
+// Contador de palabras
+let frase = prompt("Ingrese una frase: ");
+let palabras = frase.split(" ");
+let cantidadPalabras = palabras.length;
+
+console.log("La frase tiene " + cantidadPalabras + " palabras.");
